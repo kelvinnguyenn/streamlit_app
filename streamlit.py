0e75fdf73c57b72d7449eeef6538b0dbd71faa01
kelvinnguyenn <77218097+kelvinnguyenn@users.noreply.github.com>
2022-05-23 14:15:09 -0700

    Added both models
    Added regression and classification models

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,11 @@
+# Import packages
 import streamlit
 import pandas as pd
 import numpy as np
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 
 # Section 1: Dropdown Menus
 
@@ -13,8 +18,17 @@ import numpy as np
 
 
 # Section 4: Model Computation
+loaded_vectorizer = pickle.load(open('data/vectorizer.sav', 'rb'))
+loaded_classifier = pickle.load(open('data/classifier' 'rb'))
+loaded_regression = pickle.load(open('data/regression.sav', 'rb'))
 
+def classification(articles):
+    matrix = loaded_vectorizer.transform(articles)
+    return loaded_classifier.predict(matrix)
 
+def regression(articles):
+    matrix = loaded_vectorizer.transform(articles)
+    return np.exp(loaded_regression.predict(matrix) - 1)
 # Section 5: Compare Two Countries
 
 
